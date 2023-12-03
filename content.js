423d5007a9ec0a84ba1454f3c69d3786898220df
Richdevs <richardkiiluh@gmail.com>
2023-12-03 18:11:28 +0300

    Refactor sendRowCount function and remove scrollTable function

--- content.js
+++ content.js
@@ -3,26 +3,14 @@
 // Function to send a message to the popup script with the rowcount value;
 
 
-  // Your code here
-  function scrollTable() {
-    window.scrollTo(0, document.body.scrollHeight);
-  }
-//const names = []; // Declare the array outside the function
-
 function sendRowCount() {
   const targetElement = document.querySelector('[aria-colcount="1"][aria-rowcount]');
-//   const elements = document.querySelectorAll('[data-baseweb="typo-labelsmall"]');
-// const names = [];
-//   elements.forEach(element => {
-//     const name = element.textContent.trim();
-//     names.push(name);
-//   });
 
   if (targetElement) {
     const rowcountValue = targetElement.getAttribute('aria-rowcount');
-    chrome.runtime.sendMessage({ rowcount: rowcountValue, /*rowData: names */ });
+    chrome.runtime.sendMessage({ rowcount: rowcountValue});
   } else {
-    chrome.runtime.sendMessage({ rowcount: null, /*rowData: null*/ });
+    chrome.runtime.sendMessage({ rowcount: null});
   }
 }
 
@@ -52,4 +40,4 @@ observer.observe(document.body, observerConfig);
 
 // Execute the function immediately
 sendRowCount();
-scrollTable();
+
